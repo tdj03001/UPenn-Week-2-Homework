803894dd5472e653f134fadca5b37ef110c4e0df
Tyler Jenkins <tyjenki@gmail.com>
2020-01-13 17:21:55 -0500

    Added line break after emailBody to separate from things like 'sent from my iPhone'

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ $("#submitbutton").on("click", function(){
   var senderName = $("#namefield").val();
   var emailFrom = $("#emailfield").val();
   var emailBody = $("#messagefield").val();
-  emailBody = emailBody + "%0A" + "%0A" + "Sender's Name: " + senderName + " | " + " Sender's Email: " + emailFrom;
+  emailBody = emailBody + "%0A" + "%0A" + "Sender's Name: " + senderName + " | " + "  Sender's Email: " + emailFrom + "%0A";
   // $("emailBody").append(emailFrom);
-  window.location.href = "mailto:tylerdjenkins123@gmail.com?&subject=Reaching%20out&body=" + emailBody + "%0A";
+  window.location.href = "mailto:tylerdjenkins123@gmail.com?&subject=Reaching%20out&body=" + emailBody;
   
  
 });
